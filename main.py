47292023bb76b1f620f3ec8dd4deaae699044c57
maestroofmetal <joshperry2014@outlook.com>
2025-05-08 20:01:42 -0400

    Drew a triangle and turned it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
@@ -9,13 +9,29 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0   
+    
+    #draws the player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2        
+    player = Player(x, y)
+        
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        screen.fill(0000)
+        # makes the screen black
+        screen.fill("black")
+        
+        # draws the player
+        player.update(dt)
+        player.draw(screen)
+        
+        #sets the frame rate to 60 fps
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
